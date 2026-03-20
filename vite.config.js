a6a76c33133d19deab790fffc0fb85d7b7e5e7e1
katsunishi <katsunishi@keio.jp>
2026-03-20 18:00:00 +0900

    Publish static site to docs

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
     base: "/shindan/",
+    build: {
+        outDir: "docs",
+        emptyOutDir: true
+    },
     plugins: [react()],
 });
