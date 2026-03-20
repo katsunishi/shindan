8ef3e9b743ad122a18b31ca3196f274d1cac5d8e
katsunishi <katsunishi@keio.jp>
2026-03-20 17:51:34 +0900

    Fix GitHub Pages base path

--- src/router.tsx
+++ src/router.tsx
@@ -3,21 +3,26 @@ import { DiagnosePage } from "./pages/DiagnosePage";
 import { ResultPage } from "./pages/ResultPage";
 import { TypesPage } from "./pages/TypesPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <DiagnosePage />
+    },
+    {
+      path: "/result/:type",
+      element: <ResultPage />
+    },
+    {
+      path: "/types",
+      element: <TypesPage />
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" replace />
+    }
+  ],
   {
-    path: "/",
-    element: <DiagnosePage />
-  },
-  {
-    path: "/result/:type",
-    element: <ResultPage />
-  },
-  {
-    path: "/types",
-    element: <TypesPage />
-  },
-  {
-    path: "*",
-    element: <Navigate to="/" replace />
+    basename: "/shindan/"
   }
-]);
+);
